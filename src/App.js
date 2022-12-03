61af5aed813366015baefed09e60f7be1199dd51
marcelino pangilinan <marbysneaker@gmail.com>
2022-12-03 03:34:05 -0800

    started new project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css'
+
+import { Features, Footer, Blog, Possibility, WhatGPT3, Header } from './containers';
+import { Cta, Brand, Navbar,} from './components';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <Cta />
+    <Blog />
+    <Footer />
+    </div>
+  )
+}
+
+export default App
