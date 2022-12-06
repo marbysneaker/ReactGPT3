32ddb95760afd02d75e0223c1b8099e9299363ef
marcelino pangilinan <marbysneaker@gmail.com>
2022-12-05 23:21:30 -0800

    new start

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,9 +3,8 @@ import people from '../../assets/people.png';
 import ai from '../../assets/ai.png';
 import './header.css';
 
-const Header = () => {
-  return (
-    <div className="gpt3__header section__padding" id="home">
+const Header = () => (
+  <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
       <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
       <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
@@ -25,7 +24,6 @@ const Header = () => {
       <img src={ai} />
     </div>
   </div>
-  )
-}
+);
 
-export default Header
+export default Header;
