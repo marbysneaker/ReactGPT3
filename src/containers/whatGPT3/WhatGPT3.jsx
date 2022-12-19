b09347aad94d4f56132c624cc6589bc62cb9cd27
marcelino pangilinan <marbysneaker@gmail.com>
2022-12-19 02:54:16 -0800

    finished feature container

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {Feature} from '../../components/index.js';
+import Feature from '../../components/feature/Feature';
 import './whatGPT3.css';
 
-
-const WhatGPT3 = () => {
-  return (
-    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
-      <div className="gpt3__whatgpt3-feature">
+const WhatGPT3 = () => (
+  <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+    <div className="gpt3__whatgpt3-feature">
       <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
     </div>
     <div className="gpt3__whatgpt3-heading">
@@ -18,8 +16,7 @@ const WhatGPT3 = () => {
       <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
       <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
     </div>
-    </div>
-  )
-}
+  </div>
+);
 
-export default WhatGPT3
+export default WhatGPT3;
